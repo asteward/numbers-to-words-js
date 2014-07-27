051ef7697f54d5772348d30b6220bca623e716ae
asteward <austinsteward@aol.com>
2014-07-27 10:44:33 -0700

    Add support for numbers up to 8 digits in length

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,56 @@
 var numberReader = function(inputDigit) {
-  var digitValues = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four",
-                      "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine",
-                      "10": "ten", "11": "eleven", "12": "twelve", "13": "thirteen",
-                      "14": "fourteen", "15": "fifteen", "16": "sixteen", "17": "seventeen",
-                      "18": "eighteen", "19": "nineteen", "20": "twenty", "30": "thirty",
-                      "40": "forty", "50": "fifty", "60": "sixty", "70": "seventy",
-                      "80": "eighty", "90": "ninety"};
-  
-  if(inputDigit <= 20) {
-    return digitValues[inputDigit];
-  } else if (inputDigit < 100) {
-    return digitValues[inputDigit[0] * 10] + " " + numberReader(inputDigit.substr(1));
-  } else if (inputDigit < 1000) {
-    return digitValues[inputDigit[0]] + " hundred " + numberReader(inputDigit.substr(1));
-  } else if (inputDigit < 10000) {
-    return digitValues[inputDigit[0]] + " thousand " + numberReader(inputDigit.substr(1));
+  var digitValues = {0: "", 1: "one", 2: "two", 3: "three", 4: "four",
+                      5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine",
+                      10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen",
+                      14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen",
+                      18: "eighteen", 19: "nineteen", 20: "twenty", 30: "thirty",
+                      40: "forty", 50: "fifty", 60: "sixty", 70: "seventy",
+                      80: "eighty", 90: "ninety"};
+  // Removes leading zeroes
+  var tempDigitString = inputDigit.toString();
+  var tempReducedDigit = parseInt(tempDigitString);
+
+  if(tempReducedDigit <= 20) {
+    return digitValues[tempReducedDigit];
+  } else if (tempReducedDigit < 100) {
+    return digitValues[tempDigitString[0] * 10] + " " + numberReader(tempDigitString.substr(1));
+  } else if (tempReducedDigit < 1000) {
+    if (tempReducedDigit < 1) {
+      return digitValues[inputDigit[0]] + " hundred";
+    } else {
+      return digitValues[tempDigitString[0]] + " hundred " + numberReader(tempDigitString.substr(1));
+    }
+  } else if (tempReducedDigit < 10000) {
+    if (tempDigitString.substr(1) < 1) {
+      return digitValues[inputDigit[0]] + " thousand";
+    } else{
+      return digitValues[tempDigitString[0]] + " thousand " + numberReader(tempDigitString.substr(1));
+    }
+  } else if (tempReducedDigit < 100000) {
+    if (tempDigitString.substr(1) < 1) {
+      return digitValues[tempDigitString[0] * 10] + " thousand";
+    } else{
+      return digitValues[tempDigitString[0] * 10] + " " + numberReader(tempDigitString.substr(1));
+    }
+  } else if (tempReducedDigit < 1000000) {
+    if (tempDigitString.substr(1) < 1) {
+      return digitValues[tempDigitString[0]] + " hundred thousand";
+    } else{
+      return digitValues[tempDigitString[0]] + " hundred " + numberReader(tempDigitString.substr(1));
+    }
+    
+  } else if (tempReducedDigit < 10000000) {
+    if (tempDigitString.substr(1) < 1) {
+      return digitValues[tempDigitString[0]] + " million";
+    } else{
+      return digitValues[tempDigitString[0]] + " million " + numberReader(tempDigitString.substr(1));
+    }
+  } else if (tempReducedDigit < 100000000) {
+    if (tempDigitString.substr(1) < 1) {
+      return digitValues[tempDigitString[0] * 10] + " million";
+    } else{
+      return digitValues[tempDigitString[0] * 10] + " " + numberReader(tempDigitString.substr(1));
+    }
   } else {
     return "Error - Unable to read number";
   }
--- spec/specs.js
+++ spec/specs.js
@@ -2,13 +2,32 @@ describe("numberReader", function() {
   it("Returns the written form of a single digit", function() {
   numberReader("1").should.equal("one");
   });
+
   it("Returns the written form of a two digit number", function() {
   numberReader("10").should.equal("ten");
   });
+
   it("Returns the written form of a three digit number", function() {
   numberReader("559").should.equal("five hundred fifty nine");
   });
+
   it("Returns the written form of a four digit number", function() {
   numberReader("1559").should.equal("one thousand five hundred fifty nine");
+  });
+
+  it("Returns the written form of a five digit", function() {
+  numberReader(21000).should.equal("twenty one thousand");
+  });
+
+  it("Returns the written form of a six digit number", function() {
+  numberReader(100000).should.equal("one hundred thousand");
+  });
+
+  it("Returns the written form of a seven digit number", function() {
+  numberReader(3000000).should.equal("three million");
+  });
+
+  it("Returns the written form of a eight digit number", function() {
+  numberReader(46100000).should.equal("forty six million one hundred thousand");
   });
 });
